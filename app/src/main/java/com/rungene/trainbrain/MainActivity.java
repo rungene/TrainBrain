84d9ea4e6254137ecd485e58149162999f77eea9
rungene <rungenesamuel@gmail.com>
2018-05-26 22:26:20 +0300

    Adding the main activity

--- app/src/main/java/com/rungene/trainbrain/MainActivity.java
+++ app/src/main/java/com/rungene/trainbrain/MainActivity.java
@@ -2,12 +2,18 @@ package com.rungene.trainbrain;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     Button buttonGo;
+
+    TextView textViewAddition;
 
 
 
@@ -17,6 +23,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         buttonGo = findViewById(R.id.buttonGo);
+
+        textViewAddition = findViewById(R.id.textViewAddition);
+
+        Random rand = new Random();
+
+        int a = rand.nextInt(21);
+
+        int b = rand.nextInt(21);
+
+        Log.i("Random number one",Integer.toString(a));
+
+        Log.i("Random number two",Integer.toString(b));
+
+       // String c = Integer.toString(a+b);
+
+        textViewAddition.setText(Integer.toString(a));
+
+        //textViewAddition.setText(Integer.toString(b));
+
+
+
+
+
 
 
     }
